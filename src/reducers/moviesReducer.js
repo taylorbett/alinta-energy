7f157e331b32844fc09699480d14b312c7eaa281
TaylorBett <taylor.bett@hotmail.com>
2018-04-23 17:19:42 +1000

    Add basic action handling for movies reducer

--- src/reducers/moviesReducer.js
+++ src/reducers/moviesReducer.js
@@ -1,9 +1,25 @@
-const initialState = {};
+import { 
+    FETCH_MOVIES_REQUEST,
+    FETCH_MOVIES_SUCCESS,
+} from '../constants';
+
+
+const initialState = {
+    isFetching: false,
+    data: [],
+};
 
 const moviesReducer = (state = initialState, action) => {
     switch (action.type) {
-        case "FETCH_MOVIES":
-            return state;
+        case FETCH_MOVIES_REQUEST:
+            return Object.assign({}, state, {
+                isFetching: true,
+            });
+        case FETCH_MOVIES_SUCCESS:
+            return Object.assign({}, state, {
+                isFetching: false,
+                data: action.data
+            });
         default:
             return state;
     }
