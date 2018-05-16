c705dbf1fbd68a0d5557a666979ad862d26a4126
TaylorBett <taylor.bett@hotmail.com>
2018-05-16 18:50:25 +1000

    Fix typo

--- src/util/moviesUtil.js
+++ src/util/moviesUtil.js
@@ -7,22 +7,24 @@ export const sortByActor = (movies) => {
         movie.roles.forEach(role => {
             let existingEntries = output.filter(item => item.actor === role.actor);
             if (!existingEntries.length) {
-                if (role.actor !== undefined && role.actor) output.push({
-                    actor: role.actor,
-                    roles: [
-                        {
-                            movie: movie.name || '',
-                            character: role.name || '',
-                        }
-                    ]
-                });
+                if (role.actor !== undefined && role.actor) {
+                    output.push({
+                        actor: role.actor,
+                        roles: [
+                            {
+                                movie: movie.name || '',
+                                character: role.name || '',
+                            }
+                        ]
+                    });
+                }
             } else if (!existingEntries[0].roles.filter(item => item.character === role.name).length) {
                 existingEntries[0].roles.push({
                     movie: movie.name || '',
                     character: role.name || '',
                 });
             }
-        })
+        });
     });
     return output;
 };
